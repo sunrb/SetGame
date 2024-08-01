d634183d98202db3a138e0fa03af4bad7b780a65
Rubing Sun <sunjifan@gmail.com>
2024-08-01 15:19:56 -0700

    Create AspectVGrid

---
+++ SetGame/CardView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  SetGame
+//
+//  Created by Rubing on 8/1/24.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    
+    let card: Card
+    
+    var body: some View {
+        GeometryReader(content: { geometry in
+            ZStack {
+                
+            }
+        })
+    }
+}
+
+#Preview {
+    CardView()
+}
